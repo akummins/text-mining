8d5f9c4ab5ae7e4bef28efea57c0efd129578aba
Allyson Kummins <akummins2@babson.edu>
2020-10-19 23:48:30 -0400

    Create imdb.py

---
+++ imdb.py
@@ -0,0 +1,66 @@
+from imdbpie import Imdb
+import pprint
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+nltk.download('vader_lexicon')
+
+
+imdb = Imdb()
+
+endgame = imdb.get_title_user_reviews("tt4154796")
+avatar = imdb.get_title_user_reviews("tt0499549")
+titanic = imdb.get_title_user_reviews("tt0120338")
+starwars = imdb.get_title_user_reviews("tt2488496")
+infinity = imdb.get_title_user_reviews("tt4154756")
+jurassic = imdb.get_title_user_reviews("tt0369610")
+lionking = imdb.get_title_user_reviews("tt6105098")
+avengers = imdb.get_title_user_reviews("tt0848228")
+furious = imdb.get_title_user_reviews("tt2820852")
+frozen = imdb.get_title_user_reviews("tt4520988")
+movies = [endgame,avatar, titanic, starwars, infinity, jurassic ,lionking, avengers,furious, frozen]
+movies_string = ["endgame","avatar", "titanic", "starwars", "infinity","jurassic" , "lionking" , "avengers", "furious", "frozen"]
+# pprint.pprint(avengers)
+# print(avengers_reviews['reviews'][1]['author']['displayName'])
+# print(endgame['reviews'][1]['reviewText'])
+# print(len(avengers_reviews))
+
+def new_list(review):
+    all = []
+    for line in review['reviews'][1]['reviewText'].split():
+        line = line.lower()
+        all.append(line)
+    return all
+
+def histogram(reviews):
+    """returns dictionary of how many times each word in a review appears"""
+    d = dict()
+    for line in reviews:
+        if line not in d:
+            d[line] = 1
+        else:
+            d[line] = d[line]+1
+    return d
+
+def run_hists(movies):
+    for movie in movies:
+        new = new_list(movie)
+        new_hist = histogram(new)
+        print(new_hist)
+
+def main():
+    i = 0
+    for movie in movies:
+        review = movie
+        new_hist = histogram(new_list(review))
+        sort_histogram = sorted(new_hist.items(),key=lambda x: x[1], reverse = True)
+        print(movies_string[i])
+        pprint.pprint(sort_histogram)
+        score = SentimentIntensityAnalyzer().polarity_scores(review['reviews'][1]['reviewText'])
+        print(score)
+        i = i+ 1
+
+
+if __name__ == "__main__":
+    main()
+
+
